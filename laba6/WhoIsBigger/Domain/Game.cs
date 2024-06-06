b27430a287a1d0789a1f72efc7baf08b954397f3
queenhellbabe <164541083+queenhellbabe@users.noreply.github.com>
2024-06-07 00:58:28 +0500

    add laba6

---
+++ laba6/WhoIsBigger/Domain/Game.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhoIsBigger.Domain
+{
+    public class Game
+    {
+        public List<Player> Players = new List<Player>();
+        public List<Round> Rounds = new List<Round>();
+        public Round CurrentRound { get; private set; }
+
+        public bool isGameFinished { get; private set; }
+        public bool isRoundStarted { get; private set; }
+
+        public Game(int bots, int rounds)
+        {
+            if (!ValidateGameParams(bots, rounds))
+                throw new Exception("Введите количество ботов от 1 до 5\nВведите количество раундов от 1 до 10");
+
+            CreateGameSession(bots, rounds);
+        }
+
+
+
+        public void StartRound()
+        {
+            if (isRoundStarted)
+                throw new Exception("Раунд уже начат!");
+
+            int lastPlayedRoundNumber = 0;
+
+            foreach (Player player in Players)
+            {
+                if (player.Name == "Player")
+                {
+                    foreach (Dice dice in player.Dices)
+                    {
+                        if (dice.DiceRound.RoundNumber > lastPlayedRoundNumber)
+                        {
+                            lastPlayedRoundNumber = dice.DiceRound.RoundNumber;
+                        }
+                    }
+                }
+            }
+
+            foreach (Round round in Rounds)
+            {
+                if (lastPlayedRoundNumber + 1 == round.RoundNumber)
+                {
+                    CurrentRound = round;
+                    isRoundStarted = true;
+                }
+            }
+        }
+
+        public void StopGame()
+        {
+            isGameFinished = true;
+        }
+
+        public void RollDice()
+        {
+            if (!isRoundStarted)
+                throw new Exception("Раунд не начат!");
+
+            foreach (Player player in Players)
+            {
+                Dice dice = new Dice(CurrentRound);
+                dice.MakeRoll();
+                player.Dices.Add(dice);
+            }
+
+            isRoundStarted = false;
+        }
+
+        public void SaveRollResult(Player player, Dice dice)
+        {
+            if (dice.Result == 0)
+                throw new Exception(string.Format("Бросок игрока {0} не выполнен", player.Name));
+            
+            player.Dices.Add(dice);
+        }
+
+        private void CreateGameSession(int bots, int rounds)
+        {
+            Players.Add(new Player("Player"));
+
+            for (int i = 1; i <= bots; i++)
+            {
+                Player bot = new Player(string.Format("Bot{0}", i));
+
+                Players.Add(bot);
+            }
+
+            for (int i = 1; i <= rounds; i++)
+            {
+                Round round = new Round(i);
+
+                Rounds.Add(round);
+            }
+        }
+
+        private bool ValidateGameParams(int bots, int rounds)
+        {
+            return !(bots < 1 || bots > 5 || rounds < 1 || rounds > 10);
+        }
+
+        public void AddScore(Player player)
+        {
+            player.Score++;
+        }
+    }
+}
---
+++ laba6/WhoIsBigger/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WhoIsBigger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxBots = new System.Windows.Forms.TextBox();
+            this.textBoxRounds = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(153, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Создать игру";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(82, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 55);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Кто больше";
+            // 
+            // textBoxBots
+            // 
+            this.textBoxBots.Location = new System.Drawing.Point(175, 78);
+            this.textBoxBots.Name = "textBoxBots";
+            this.textBoxBots.Size = new System.Drawing.Size(166, 20);
+            this.textBoxBots.TabIndex = 2;
+            // 
+            // textBoxRounds
+            // 
+            this.textBoxRounds.Location = new System.Drawing.Point(175, 115);
+            this.textBoxRounds.Name = "textBoxRounds";
+            this.textBoxRounds.Size = new System.Drawing.Size(166, 20);
+            this.textBoxRounds.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Боты";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(112, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Раунды";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 245);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxBots);
+            this.Controls.Add(this.textBoxRounds);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Кто больше";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxBots;
+        private System.Windows.Forms.TextBox textBoxRounds;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
